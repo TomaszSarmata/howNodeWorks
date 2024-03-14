3ff327c2eb92b1c070b3cc8af7f0a7b18dbc8354
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-14 14:55:20 +0000

    Used the pipe method on the readable to consume the stram data without any back pressure

--- streams.js
+++ streams.js
@@ -7,19 +7,24 @@ server.on("request", (req, res) => {
   //   if (err) console.log(err);
   //   res.end(data);
   // });
-  //Solution 2 - streams
-  const readable = fs.createReadStream("test-file.txt"); //that will produce chunks of data that are ready to consume and the stream emits then data event. So we can attach our listener on readable to listen to that event.
-  readable.on("data", (chunk) => {
-    res.write(chunk); //res is a writteable stream and so we can stream the content right to the client using the cb function from the .on listener. The res comes from the stram functonality.
-  });
-  readable.on("end", () => {
-    res.end(); //that signals that no moro data will be written into the stream
-  });
-  readable.on("error", (err) => {
-    console.log(err);
-    res.statusCode = 500;
-    res.end("File not found");
-  });
+  //   //Solution 2 - streams
+  //   const readable = fs.createReadStream("test-file.txt"); //that will produce chunks of data that are ready to consume and the stream emits then data event. So we can attach our listener on readable to listen to that event.
+  //   readable.on("data", (chunk) => {
+  //     res.write(chunk); //res is a writteable stream and so we can stream the content right to the client using the cb function from the .on listener. The res comes from the stram functonality.
+  //   });
+  //   readable.on("end", () => {
+  //     res.end(); //that signals that no moro data will be written into the stream
+  //   });
+  //   readable.on("error", (err) => {
+  //     console.log(err);
+  //     res.statusCode = 500;
+  //     res.end("File not found");
+  //   });
+  // Solution 3 - that will sort out the back pressure that we were getting with the solution 2
+
+  const readable = fs.createReadStream("test-file.txt");
+  readable.pipe(res); //here we are simply piping the response
+  // so the rule is: readableSource.pipe(wiriteableDestination)
 });
 
 server.listen(8000, "localhost", () => {
