e6772ca9fadd6177d3da50e5639cbc228ca4bda2
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-14 15:29:28 +0000

    Imported our calculator class and created an instance of it to use the functionality of our calculator

--- modules.js
+++ modules.js
@@ -1,2 +1,8 @@
 // console.log(arguments);
 // console.log("here!!!", require("module").wrapper);
+
+const CanBeAnyName = require("./test-module-1"); //saving here the return value of the module.exports which will evaluate to our class
+
+const calc1 = new CanBeAnyName(); // creating a new instance of a calculator
+console.log(calc1.add(1, 2));
+console.log(calc1.multiply(5, 10));
