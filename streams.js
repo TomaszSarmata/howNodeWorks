d6290a728d02250e0a323b5420668033402a1afb
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-14 14:34:38 +0000

    Created a readable stream to demonstrate how we can send to the client small chunks of data instead of soring the whole piece of the info in the memory / variable and then sending to the client

--- streams.js
+++ streams.js
@@ -3,9 +3,14 @@ const server = require("http").createServer();
 
 server.on("request", (req, res) => {
   //Solution 1
-  fs.readFile("test-file.txt", (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
+  // fs.readFile("test-file.txt", (err, data) => {
+  //   if (err) console.log(err);
+  //   res.end(data);
+  // });
+  //Solution 2 - streams
+  const readable = fs.createReadStream("test-file.txt"); //that will produce chunks of data that are ready to consume and the stream emits then data event. So we can attach our listener on readable to listen to that event.
+  readable.on("data", (chunk) => {
+    res.write(chunk); //res is a writteable stream and so we can stream the content right to the client using the cb function from the .on listener
   });
 });
 
