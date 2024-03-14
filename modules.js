32a75ac4f45db736fff311c504c735f16d7312fb
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-14 15:41:24 +0000

    Created a new file to export anynymous functions that we assign to the exports object. Then imported the file and saved the returned value (export object) in a variable called calc2

--- modules.js
+++ modules.js
@@ -1,8 +1,16 @@
 // console.log(arguments);
 // console.log("here!!!", require("module").wrapper);
 
+// MODULE.EXPORTS
+///////////////////
 const CanBeAnyName = require("./test-module-1"); //saving here the return value of the module.exports which will evaluate to our class
 
 const calc1 = new CanBeAnyName(); // creating a new instance of a calculator
 console.log(calc1.add(1, 2));
 console.log(calc1.multiply(5, 10));
+//////////////////////
+
+//EXPORTS
+//we are using the shorthand here to achieve the same as above
+const calc2 = require("./test-module-2"); //calc2 is the export object here
+console.log(calc2.add(2, 5), calc2.multiply(2, 5));
---
+++ test-module-2.js
@@ -0,0 +1,4 @@
+//created three anonymous functions and assigned them to three properties of exports
+exports.add = (a, b) => a + b;
+exports.multiply = (a, b) => a * b;
+exports.divide = (a, b) => a / b;
