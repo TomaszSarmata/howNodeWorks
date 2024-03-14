02bbd7ad29ec6b085e96a2a35dee2d12c5506235
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-14 14:45:18 +0000

    Generated an  error to test

--- streams.js
+++ streams.js
@@ -8,7 +8,7 @@ server.on("request", (req, res) => {
   //   res.end(data);
   // });
   //Solution 2 - streams
-  const readable = fs.createReadStream("test-file.txt"); //that will produce chunks of data that are ready to consume and the stream emits then data event. So we can attach our listener on readable to listen to that event.
+  const readable = fs.createReadStream("testttt-file.txt"); //that will produce chunks of data that are ready to consume and the stream emits then data event. So we can attach our listener on readable to listen to that event.
   readable.on("data", (chunk) => {
     res.write(chunk); //res is a writteable stream and so we can stream the content right to the client using the cb function from the .on listener. The res comes from the stram functonality.
   });
@@ -17,7 +17,7 @@ server.on("request", (req, res) => {
   });
   readable.on("error", (err) => {
     console.log(err);
-    res.status(500);
+    res.statusCode = 500;
     res.end("File not found");
   });
 });
