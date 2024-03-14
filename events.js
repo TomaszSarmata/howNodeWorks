984097596352d82183524dd9c8aa90a83529c0de
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-14 09:45:16 +0000

    set up event listeners for the newSale event

--- events.js
+++ events.js
@@ -1,3 +1,14 @@
 const EventEmitter = require("events");
 
 const myEmitter = new EventEmitter();
+
+//setting up listeners
+myEmitter.on("newSale", () => {
+  console.log("there was a new sale");
+});
+
+myEmitter.on("newSale", () => {
+  console.log("customer name: Tomasz");
+});
+
+myEmitter.emit("newSale");
